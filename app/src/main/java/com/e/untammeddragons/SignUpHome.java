6639d7989a82056bd162323dc43aeafbc5261fef
shubhansu31 <jainshubhansu31@gmail.com>
2020-08-23 21:05:38 +0530

    Register page incomplete

--- app/src/main/java/com/e/untammeddragons/SignUpHome.java
+++ app/src/main/java/com/e/untammeddragons/SignUpHome.java
@@ -17,28 +17,28 @@ import com.google.firebase.auth.AuthResult;
 public class SignUpHome extends AppCompatActivity {
 
     Button register;
+    EditText name , email , mnumber , password , cpassword;
+    String sname , semail , smnumber , spassword , scpassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_home);
+        name = (EditText)findViewById(R.id.edit_Text_name);
+        email = (EditText)findViewById(R.id.edit_Text_email);
+        mnumber = (EditText)findViewById(R.id.edit_Text_mnumber);
+        password = (EditText)findViewById(R.id.edit_Text_password);
+        cpassword = (EditText)findViewById(R.id.edit_Text_cpassword);
+
         register = (Button)findViewById(R.id.register);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                email = inemail.getText().toString();
-//                password = inpassword.getText().toString();
-//                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignInHome.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if(task.isSuccessful()){
-//                            startActivity(new Intent(SignInHome.this,Home.class));
-//                            Toast.makeText(SignInHome.this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
-////                            finish();
-//                        }
-//                        else
-//                            Toast.makeText(SignInHome.this,"Failed to Register" , Toast.LENGTH_LONG).show();
+                sname = name.getText().toString();
+                semail = email.getText().toString();
+                smnumber = mnumber.getText().toString();
+                scpassword = cpassword.getText().toString();
+                spassword = password.getText().toString();
             }
         });
-
     }
 }
